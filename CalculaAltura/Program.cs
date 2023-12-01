99fe03a473d5e581d65c55e9e99805525fe90bd2
BrunaGPires <bruna.gpires@hotmail.com>
2023-12-01 01:32:44 -0300

    first commit

---
+++ CalculaAltura/Program.cs
@@ -0,0 +1,6 @@
+﻿/*
+Desenvolver um algoritmo que leia a altura de 15 pessoas. Este programa deverá calcular e
+mostrar :
+a. A menor altura do grupo;
+b. A maior altura do grupo;
+*/
---
+++ SomaNumerosImpares/Program.cs
@@ -0,0 +1,33 @@
+﻿int[] numeros = Enumerable.Range(1, 500).ToArray();
+int[] impares = FiltroImpares(numeros);
+
+Console.WriteLine("\nNumeros ímpares múltiplos de três: ");
+foreach (int num in impares)
+{
+    Console.Write(num + " ");
+}
+
+Console.WriteLine("\n\nA soma dos números ímpares múltiplos de três de 1 a 500 é: " + SomaImpares(impares));
+
+int[] FiltroImpares (int[] numeros)
+{
+    var filtrados = new List<int>();
+    foreach (int num in numeros)
+    {
+        if (num % 2 != 0 && num % 3 == 0)
+        {
+            filtrados.Add(num);
+        }
+    }
+    return filtrados.ToArray();
+}
+
+int SomaImpares (int[] impares)
+{
+    int soma = 0;
+    foreach (int valor in impares)
+    {
+        soma += valor;
+    }
+    return soma;
+}
